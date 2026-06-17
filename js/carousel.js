95604f1659ddd76604ef8ac6672e9e81b1ecb5b2
siderilla <siderilla11@gmail.com>
2026-06-18 01:09:59 +0200

    template is ok

--- js/carousel.js
+++ js/carousel.js
@@ -1,36 +1,12 @@
 let currentIndex = 0;
 
-function renderCard(project) {
-	const tags = project.tags
-		.map(tag => `<span class="tag">[ ${tag} ]</span>`)
-		.join('')
-
-	return `
-			<div class="card" data-link="${project.link}">
-			<div class="card-status">// ${project.status}</div>
-			<div class="card-thumbnail">
-				${project.thumbnail
-			? `<img src="${project.thumbnail}" alt="${project.title}">`
-			: `<div class="card-thumbnail-placeholder">[ NO SIGNAL ]</div>`
-		}
-			</div>
-			<h3 class="card-title">${project.title}</h3>
-			<p class="card-description">${project.description}</p>
-			<div class="card-tags">${tags}</div>
-			<button class="project-link">[ ENTER_LOG ]</button>
-		</div>
-	`
-
-}
-
 function updateCarousel() {
-	const cardContainer = document.getElementById('card-container');
-	cardContainer.innerHTML = renderCard(projects[currentIndex]);
+	const cardContainer = document.getElementById('card-container')
+	cardContainer.innerHTML = cardTemplate(projects[currentIndex])
 
-	//click sulla card o sul bottone -> naviga
 	cardContainer.querySelector('.card').addEventListener('click', () => {
-		window.location.href = projects[currentIndex].link;
-	});
+		window.location.href = projects[currentIndex].link
+	})
 }
 
 document.querySelector('.arrow-left').addEventListener('click', () => {
